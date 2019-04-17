e9a6d52c14594abef098a6052cfbd0394eb898a0
j-mir-prazak <j.mir.prazak@gmail.com>
2019-04-17 17:20:04 +0200

    quick git

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function handletty(tty) {
 }
 
 function pdl2ork() {
-	var pd = spawner.spawn("bash", new Array("-c", "pd-l2ork -open pd/pardu_player.pd -send \"ard "+ arduino + "\""), {detached: true})
+	var pd = spawner.spawn("DISPLAY=:1", new Array("bash", "-c", "pd-l2ork -open pd/pardu_player.pd -send \"ard "+ arduino + "\""), {detached: true})
 	var decoder = new StringDecoder('utf-8')
 	pids.push(pd["pid"])
 
@@ -154,7 +154,7 @@ function pdl2ork() {
 // qlc()
 
 function qlcplus() {
-	var qlc = spawner.spawn("bash", new Array("-c", "qlcplus -o ./scene.qxw -p"), {detached: true})
+	var qlc = spawner.spawn("DISPLAY=:1", new Array("bash", "-c", "qlcplus -o ./scene.qxw -p"), {detached: true})
 	var decoder = new StringDecoder('utf-8')
 	pids.push(qlc["pid"])
 
